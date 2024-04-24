d37c72a1ffdd9eb79c6f370b86ce3ff2b2d80d1c
Tharani404 <164305986+Tharani404@users.noreply.github.com>
2024-04-24 20:13:23 +0530

    Create post pane

---
+++ src/Components/AddPost/AddPost.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import "./addPost.css"
+import InsertPhotoIcon from '@mui/icons-material/InsertPhoto';
+import AddLocationAltIcon from '@mui/icons-material/AddLocationAlt';
+import LocalOfferIcon from '@mui/icons-material/LocalOffer';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+
+export default function AddPost() {
+  return (
+    <div className='addPost'>
+        <div className="addPostContainer">
+            <div className="addPostTop">
+                <img src="/images/1.jpg" alt="" className="addPostPic" />
+                <input placeholder='whats in your mind..?' type="text" className="addPostInput" />
+            </div>
+            <hr className='postHr'/>
+            <div className="addPostBottom">
+                <div className="addPostOptions">
+                    <div className="addPostOption">
+                        <InsertPhotoIcon className='addPhoto'/>
+                        <span className="addPostOptionText">Add Photo/Video</span>
+                    </div>
+                    <div className="addPostOption">
+                        <AddLocationAltIcon className='addPhoto'/>
+                        <span className="addPostOptionText">Add Location</span>
+                    </div>
+                    <div className="addPostOption">
+                        <LocalOfferIcon className='addPhoto'/>
+                        <span className="addPostOptionText">Add Tag</span>
+                    </div>
+                    <div className="addPostOption">
+                        <LiveTvIcon className='addPhoto'/>
+                        <span className="addPostOptionText">Go Live</span>
+                    </div>
+                </div>
+                <button className="buttonPost">Post</button>
+            </div>
+        </div>
+    </div>
+  )
+}
